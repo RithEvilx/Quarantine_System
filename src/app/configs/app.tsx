69f55a92c7dcf3fd133cf584cad6ec78a81d0ac4
TempEvil <tempevilx@gmail.com>
2026-09-08 09:53:24 +0700

    Forgot other one

--- src/app/configs/app.tsx
+++ src/app/configs/app.tsx
@@ -2,6 +2,8 @@ import FallBackImage from "/imgs/fallback_image.webp";
 
 export const fallBackImage = FallBackImage;
 
+export const APP_API_URL = import.meta.env.VITE_APP_API_URL;
+
 export const APP_NAME = import.meta.env.VITE_APP_NAME;
 
 export const APP_SECRET = import.meta.env.VITE_APP_SECRET;
